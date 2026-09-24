2537766639850c9fe29c4949f5fa484358f42806
105558371 <105558371@student.swin.edu.au>
2026-09-24 13:29:42 +0700

    Restructure folders

---
+++ Week 2/Computer Lab/Computer Lab/Computer_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Computer_LabApp.swift
+//  Computer Lab
+//
+//  Created by SESEIU24007 on 24/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Computer_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
