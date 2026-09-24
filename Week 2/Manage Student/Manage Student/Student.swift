506638f716ee51c5a75722893c3a56a73f01f75e
105558371 <105558371@student.swin.edu.au>
2026-09-24 15:24:21 +0700

    Initialize Student array with data

--- Week 2/Manage Student/Manage Student/Student.swift
+++ Week 2/Manage Student/Manage Student/Student.swift
@@ -5,3 +5,12 @@ struct Student: Identifiable {
     var name: String
     var gpa: Double
 }
+
+// Initialize Array with Initial Data
+var students: [Student] = [
+    Student(id: "S001", name: "An", gpa: 8.5),
+    Student(id: "S002", name: "Binh", gpa: 7.8),
+    Student(id: "S003", name: "Chi", gpa: 9.2),
+    Student(id: "S004", name: "Duy", gpa: 9.2),
+    Student(id: "S005", name: "Lan", gpa: 8.0)
+]
