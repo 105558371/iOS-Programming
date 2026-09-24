577727ccc54704b00fcfd87bf44a5c5f2718c1d3
105558371 <105558371@student.swin.edu.au>
2026-09-24 15:09:57 +0700

    Define Student Struct

---
+++ Week 2/Manage Student/Manage Student/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Manage Student
+//
+//  Created by SESEIU24007 on 24/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Week 2/Manage Student/Manage Student/Manage_StudentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Manage_StudentApp.swift
+//  Manage Student
+//
+//  Created by SESEIU24007 on 24/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Manage_StudentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
