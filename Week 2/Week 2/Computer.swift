72d294af82e17295eca6e935d3103bb9f8d33423
105558371 <105558371@student.swin.edu.au>
2026-09-24 13:30:12 +0700

    Delete Week 2/Week 2 directory

--- Week 2/Week 2/Computer.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct Computer: Identifiable {
-    let id = UUID()         // Unique identifier
-    var name: String        // Computer name (e.g., "PC06")
-    var location: String    // Location (e.g., "Lab A")
-    var isAvailable: Bool   // Status in lab
-}
